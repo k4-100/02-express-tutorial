3248b20e78efc6f58d87bed00fd61894b7dd0514
k4-100 <k4-100@protonmail.com>
2022-08-25 14:46:00 +0200

    Some basic code

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const HOSTNAME = "127.0.0.1";
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("response has been sent");
+  console.log(req.url);
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>response has been sent</h1>");
+  res.end();
 });
 
 server.listen(PORT, HOSTNAME, () => {
-  console.log(`LISTENING ${HOSTNAME}:${PORT}`);
+  console.log(`LISTENING at ${HOSTNAME}:${PORT}`);
 });
