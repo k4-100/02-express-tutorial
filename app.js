3351e7c48f4657e13dc86230e3c3ccb233512fd9
k4-100 <k4-100@protonmail.com>
2022-08-30 15:02:07 +0200

    url params

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const { products, people } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json([{ name: "john" }, { name: "susan" }]);
+  res.send('<h1> Home Page </h1> <a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json([newProducts]);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const singleProduct = products.find(
+    (product) => String(product.id) === req.params.id
+  );
+  res.json(singleProduct || "failed to fetch data");
 });
 
 app.listen(5000, () => {
