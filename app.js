1ae29a4df735cf6771776605afdc70b0c4c8722b
k4-100 <k4-100@protonmail.com>
2022-08-31 17:02:43 +0200

    POST (HTML)

--- app.js
+++ app.js
@@ -1,47 +1,23 @@
 const express = require("express");
 const app = express();
-const { products, people } = require("./data");
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send('<h1> Home Page </h1> <a href="/api/products">products</a>');
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json([newProducts]);
-});
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) return res.status(200).send(`Welcome ${name}`);
 
-// url param
-app.get("/api/products/:id", (req, res) => {
-  const singleProduct = products.find(
-    (product) => String(product.id) === req.params.id
-  );
-  res.json(singleProduct || "failed to fetch data");
-});
-
-// query param
-app.get("/api/v1/query", (req, res) => {
-  console.log(req.query);
-  const { search } = req.query;
-  const limit = Number(req.query.limit);
-  console.log(limit);
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) =>
-      product.name.startsWith(search)
-    );
-  }
-  if (limit <= sortedProducts.length) {
-    sortedProducts = sortedProducts.slice(0, limit);
-  }
-
-  res.json(sortedProducts);
+  res.status(401).send("Please Provide Credentials");
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000....");
+  console.log("Server is listening on port 5000....");
 });
